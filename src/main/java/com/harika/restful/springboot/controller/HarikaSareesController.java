d8d8fad115f1be179bfbcd018f8aab4b96a3bee0
Harika <harikamutyam@gmail.com>
2023-03-04 20:30:06 -0800

    initial setup for springboot

---
+++ src/main/java/com/harika/restful/springboot/controller/HarikaSareesController.java
@@ -0,0 +1,28 @@
+package com.harika.restful.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by harika on 3/4/23.
+ */
+@RestController
+public class HarikaSareesController {
+
+    @GetMapping("/getSarees")
+    public String getSareesFromBackend() {
+        // ddb call
+        return "Sarees endpoint";
+    }
+
+    @GetMapping("/getPrice")
+    public String getPriceFromBackend() {
+        // ddb call
+        return "pricing endpoint";
+    }
+
+
+
+
+}
